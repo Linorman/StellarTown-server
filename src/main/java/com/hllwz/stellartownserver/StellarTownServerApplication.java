0f170ace4ec8c2a55c1009229d5598def85abd5d
Linorman <3033797357@qq.com>
2023-06-19 15:42:50 +0800

    Initial commit

---
+++ src/main/java/com/hllwz/stellartownserver/StellarTownServerApplication.java
@@ -0,0 +1,13 @@
+package com.hllwz.stellartownserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarTownServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StellarTownServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hllwz/stellartownserver/StellarTownServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hllwz.stellartownserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarTownServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
