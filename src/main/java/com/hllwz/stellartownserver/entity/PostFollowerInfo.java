4bd79869b45af3a6c97db374557da7068cd77c41
Linorman <3033797357@qq.com>
2023-06-20 17:18:04 +0800

    feat: 修改实体类

--- src/main/java/com/hllwz/stellartownserver/entity/PostFollowerInfo.java
+++ src/main/java/com/hllwz/stellartownserver/entity/PostFollowerInfo.java
@@ -1,10 +1,14 @@
 package com.hllwz.stellartownserver.entity;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 public class PostFollowerInfo implements Serializable {
@@ -13,4 +17,7 @@ public class PostFollowerInfo implements Serializable {
     private Integer postId;
     private String followerId;
     private int delFlag;
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    @TableField(value = "follow_time", fill = FieldFill.INSERT)
+    private Date followTime;
 }
