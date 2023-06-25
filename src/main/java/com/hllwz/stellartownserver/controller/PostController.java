6cea67b97e2a13179be7c8218dc756d7762daff2
liuyunhao <1271359835@qq.com>
2023-06-25 21:04:44 +0800

    docs:注释修改

--- src/main/java/com/hllwz/stellartownserver/controller/PostController.java
+++ src/main/java/com/hllwz/stellartownserver/controller/PostController.java
@@ -2,39 +2,60 @@ package com.hllwz.stellartownserver.controller;
 
 import com.hllwz.stellartownserver.common.ResponseResult;
 import com.hllwz.stellartownserver.entity.PostInfo;
-import com.hllwz.stellartownserver.entity.UserInfo;
 import com.hllwz.stellartownserver.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 帖子控制器
+ * @author Lyh
+ * @version 1.0.0
+ */
 @Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("/forum")
 public class PostController {
-     @Autowired
-     private PostService postService;
+    @Autowired
+    private PostService postService;
 
-     @GetMapping("/getPost")
-    public ResponseResult getPost(@RequestBody PostInfo postInfo){
+    @GetMapping("/getPost")
+    /**
+     * 获取帖子
+     * @return ResponseResult
+     */
+    public ResponseResult getPost(@RequestBody PostInfo postInfo) {
+        return postService.getPost(postInfo);
+    }
 
-         return postService.getPost(postInfo);
-     }
-     @GetMapping("/getAllPosts")
-    public ResponseResult getAllPosts(){
-         return postService.getAllPosts();
-     }
-     @PostMapping("/post")
-    public ResponseResult post(@RequestBody PostInfo postInfo ){
-         return  postService.post(postInfo);
-     }
-     @PostMapping("/deletePost")
-    public ResponseResult deletePost(@RequestBody PostInfo postInfo ){
-         return  postService.deletePost(postInfo);
-     }
+    /**
+     * 获取全部帖子
+     * @return ResponseResult
+     */
+    @GetMapping("/getAllPosts")
+    public ResponseResult getAllPosts() {
+        return postService.getAllPosts();
+    }
 
+    /**
+     * 发帖
+     * @return ResponseResult
+     */
+    @PostMapping("/post")
+    public ResponseResult post(@RequestBody PostInfo postInfo) {
+        return postService.post(postInfo);
+    }
 
+    /**
+     * 删帖
+     *
+     * @return ResponseResult
+     */
+    @PostMapping("/deletePost")
+    public ResponseResult deletePost(@RequestBody PostInfo postInfo) {
+        return postService.deletePost(postInfo);
+    }
 
 
 }
--- src/main/java/com/hllwz/stellartownserver/controller/PostFollowerController.java
+++ src/main/java/com/hllwz/stellartownserver/controller/PostFollowerController.java
@@ -1,4 +1,5 @@
 package com.hllwz.stellartownserver.controller;
+
 import com.hllwz.stellartownserver.common.ResponseResult;
 import com.hllwz.stellartownserver.entity.PostFollowerInfo;
 import com.hllwz.stellartownserver.service.PostFollowerService;
@@ -6,6 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 帖子点赞控制器
+ *
+ * @author Lyh
+ * @version 1.0.0
+ */
 @Slf4j
 @CrossOrigin
 @RestController
@@ -14,9 +21,13 @@ public class PostFollowerController {
     @Autowired
     private PostFollowerService postFollowerService;
 
+    /**
+     * 返回点赞数
+     * @return ResponseResult
+     */
     @GetMapping("/getLikes")
-    public ResponseResult getLikes(@RequestBody PostFollowerInfo postFollowerInfo){
-        return  postFollowerService.getLikes(postFollowerInfo);
+    public ResponseResult getLikes(@RequestBody PostFollowerInfo postFollowerInfo) {
+        return postFollowerService.getLikes(postFollowerInfo);
     }
 
 
